4f268f2b28c24a03547cd5b0891a71a8ce8371b9
Markus Gray <syncwarellc@gmail.com>
2024-12-02 14:18:09 -0500

    fix: banner styles

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -7,9 +7,9 @@ export default function Banner() {
 
     if(path == '/'){
         return (
-            <div className="block bg-primary-700 py-8">
+            <div className="block bg-primary-700 py-4">
                 <div className="container mx-auto">
-                    <h5 className='text-center text-white '>Does your team need reliable design and dev support? Checkout our new <a href="https://ontap.syncwaretechnologies.com">On-Tap</a> service </h5>
+                    <h5 className='text-center text-white font-display tracking-wide'>Does your team need reliable Design and Development support? Checkout our new <a href="https://ontap.syncwaretechnologies.com" className="hover:underline underline-offset-2">OnTap</a> service!</h5>
                 </div>
             </div>
         )
